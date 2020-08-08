a1244cccc9a8298b9e5550d7c13e77a3309368b0
Murad Atta <murad.atta@gmail.com>
2020-08-08 12:54:39 -0400

    fixing seed.sql

--- seeds.sql
+++ seeds.sql
@@ -8,4 +8,4 @@ INSERT INTO role (id,title,salary,department_id)
 VALUES ('1','manager','90000.00','1'),('2','developer','90000.00','2'), ('3','Engineer','90000.00','3'), ('4','costumer rep','50000.00','4');
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ('1', 'John', 'Doe', '1', null), ('2', 'Jack', 'Smith', '2', '1'),('3', 'Jack', 'Dear', '13', '1'),('4', 'Jane', 'Doe', '4', '1');
+VALUES ('1', 'John', 'Doe', '1', null), ('2', 'Jack', 'Smith', '2', '1'),('3', 'Joe', 'Dear', '3', '1'),('4', 'Jane', 'Doe', '4', '1');
